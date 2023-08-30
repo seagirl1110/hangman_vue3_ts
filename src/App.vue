d594ac86a699a3cfdd6bbbb834ffc46a1776145a
Marina Elizarova <seagirl1110@gmail.com>
2023-08-30 21:53:41 +0300

    feat: add game word

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,17 @@ import GameWrongLetters from './components/GameWrongLetters.vue'
 import GameWord from './components/GameWord.vue'
 import GamePopup from './components/GamePopup.vue'
 import GameNotification from './components/GameNotification.vue'
+import { computed, ref } from 'vue'
 
+const word = ref('василий')
+const letters = ref<string[]>([])
+const correctLetters = computed(() => letters.value.filter(letter => word.value.includes(letter)))
+
+window.addEventListener('keydown', ({ key }) => {
+    if (/[а-яА-ЯёЁ]/.test(key)) {
+        letters.value.push(key.toLowerCase())
+    }
+})
 </script>
 
 <template>
@@ -14,7 +24,7 @@ import GameNotification from './components/GameNotification.vue'
     <div class="game-container">
         <GameFigure />
         <GameWrongLetters />
-        <GameWord />
+        <GameWord :word="word" :correct-letters="correctLetters" />
     </div>
 
     <GamePopup v-if="false" />
