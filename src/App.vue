a823e3b5b8c3556cca1fc6bdc8a20d1ba627b9b8
Marina Elizarova <seagirl1110@gmail.com>
2023-08-30 19:50:11 +0300

    feat: add components and style

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import GameHeader from './components/GameHeader.vue'
+import GameFigure from './components/GameFigure.vue'
+import GameWrongLetters from './components/GameWrongLetters.vue'
+import GameWord from './components/GameWord.vue'
+import GamePopup from './components/GamePopup.vue'
+import GameNotification from './components/GameNotification.vue'
+
+</script>
+
+<template>
+    <GameHeader />
+
+    <div class="game-container">
+        <GameFigure />
+        <GameWrongLetters />
+        <GameWord />
+    </div>
+
+    <GamePopup v-if="false" />
+    <GameNotification />
+</template>
---
+++ src/components/GameWrongLetters.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="wrong-letters-container">
+        <div class="wrong-letters">
+            <p>Ошибки</p>
+            <span>с , </span>
+            <span>м , </span>
+            <span>п </span>
+        </div>
+    </div>
+</template>
